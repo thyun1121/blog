86ba201a568f86bc45d680aa9f41c89191530a38
thyun1121 <thyun.ahn@gmail.com>
2017-02-13 15:10:33 +0000

    apply daou recruit crawling

--- blog/static/js/crawling.js
+++ blog/static/js/crawling.js
@@ -1,6 +1,7 @@
 $(window).load(function() {     
 	$.getJSON('/root/tedProject/blog/static/item.json', function(data) {
 		var html = '';
+			html += '<h2>'+data[0].source+'</h2>'
 		$.each(data, function(index, val) {
 			html += '<div class="entry">';
 			html += '<h3 class="term">' + val.title+ '</h3>';
